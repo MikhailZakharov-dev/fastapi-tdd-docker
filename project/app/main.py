7045eb44c9dfd0f1e446eb11b12d62c89a7261f4
zakharov mikhail <zakharovmikhail@zakharovs-MacBook-Pro.local>
2025-11-16 22:39:03 +0300

    refactor(app): improve app structure
    use app router, db init, schema generate in separated modules

--- project/app/main.py
+++ project/app/main.py
@@ -2,22 +2,23 @@ from fastapi import FastAPI, Depends
 from app.config import get_settings, Settings
 from tortoise.contrib.fastapi import register_tortoise
 import os
+from app.api import ping
+from app.db import init_db
+def create_application() -> FastAPI:
+  application = FastAPI()
+  
+  register_tortoise(
+    application,
+    db_url=os.environ.get("DATABASE_URL"),
+    modules={"models": ["app.models.tortoise"]},
+    generate_schemas=True,
+    add_exception_handlers=True
+  )
+  
+  application.include_router(ping.router)
+  
+  return application
 
-kek = FastAPI()
-
-
-register_tortoise(
-  kek,
-  db_url=os.environ.get("DATABASE_URL"),
-  modules={"models": ["app.models.tortoise"]},
-  generate_schemas=True,
-  add_exception_handlers=True
-)
-
-@kek.get("/ping")
-async def pong(settings: Settings = Depends(get_settings)):
-    return {
-      "ping": "pong!",
-      "environment": settings.environment,
-      "testing": settings.testing
-    }
+kek = create_application()
+  
+init_db(kek)
