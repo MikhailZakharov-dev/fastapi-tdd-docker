42cca1e849fa86b4100996f89b4abbdb65862ca9
zakharov mikhail <zakharovmikhail@zakharovs-MacBook-Pro.local>
2025-11-16 21:41:45 +0300

    feat(postgre): added db and docker compose to spin up services

--- project/app/config.py
+++ project/app/config.py
@@ -1,12 +1,14 @@
 import logging
 from functools import lru_cache
 from pydantic_settings import BaseSettings
+from pydantic import AnyUrl
 
 log = logging.getLogger("uvicorn");
 
 class Settings(BaseSettings):
   environment: str = "dev"
   testing: bool = bool(0)
+  database_url: AnyUrl = None # pyright: ignore[reportAssignmentType]
   
 @lru_cache()
 def get_settings() -> BaseSettings:
--- project/app/main.py
+++ project/app/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI, Depends
 from app.config import get_settings, Settings
+from tortoise.contrib.fastapi import register_tortoise
+import os
 
 kek = FastAPI()
+
+
+register_tortoise(
+  kek,
+  db_url=os.environ.get("DATABASE_URL"),
+  modules={"models": ["app.models.tortoise"]},
+  generate_schemas=True,
+  add_exception_handlers=True
+)
 
 @kek.get("/ping")
 async def pong(settings: Settings = Depends(get_settings)):
