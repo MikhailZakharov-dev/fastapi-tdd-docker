486431fc4e5e618bb6fe87102743ab9ed300d44f
zakharov mikhail <zakharovmikhail@zakharovs-MacBook-Pro.local>
2025-11-22 22:03:25 +0300

    feat(summaries): add validation url and id paramaters

--- project/tests/conftest.py
+++ project/tests/conftest.py
@@ -14,20 +14,19 @@ def get_settings_override():
     return Settings(testing=1, database_url=os.environ.get("DATABASE_TEST_URL"))
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="function")
 def test_app():
     # set up
-
     app = create_application()
     app.dependency_overrides[get_settings] = get_settings_override
     with TestClient(app) as test_client:
         # testing
         yield test_client
-
     # tear down
+    app.dependency_overrides.clear()
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="function")
 def test_app_with_db():
     app = create_application()
     app.dependency_overrides[get_settings] = get_settings_override
